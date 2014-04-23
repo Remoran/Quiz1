be7d9ae7aa30c8fbb563fabb236944abb7e8cb1a
Remoran <ryan@mmoran.com>
2014-04-23 10:55:27 -0400

    Quiz1

--- Quiz/QuizViewController.h
+++ Quiz/QuizViewController.h
@@ -8,6 +8,25 @@
 
 #import <UIKit/UIKit.h>
 
+
 @interface QuizViewController : UIViewController
+{
+    int currentQuestionIndex;
+    
+    // The model objects
+    NSMutableArray *questions;
+    NSMutableArray *answers;
+    
+    // The view objects
+    IBOutlet UILabel *questionField;
+    IBOutlet UILabel *answerField;
+}
+
+- (IBAction)showQuestion:(id)sender;
+@property (weak, nonatomic) IBOutlet UILabel *questionLabel;
+
+- (IBAction)showAnswer:(id)sender;
+@property (weak, nonatomic) IBOutlet UILabel *answerLabel;
+
 
 @end
